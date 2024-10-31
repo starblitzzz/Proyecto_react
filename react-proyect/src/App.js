0d85d6a0b6d627a082b12f0187f53e26de730caa
starblitzzz <santiagorodriguezsuarez2004@gmail.com>
2024-10-30 21:58:04 -0500

    modifiacion del archivo data.js y request

--- react-proyect/src/App.js
+++ react-proyect/src/App.js
@@ -2,6 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  fetch("./data.json")
+  .then(response => {
+    if (!response.ok) { 
+      throw new Error("Hubo un problema con la información");
+    }
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(response.json()); 
+      }, getRandomInRange(2000, 4000));
+    });
+  })
+    
+    
+  
+  .then((data)=>console.log(data))
+  .catch(error => {
+    console.error("Hubo un problema con la solicitud:", error);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
